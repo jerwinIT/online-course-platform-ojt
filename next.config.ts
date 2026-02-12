a0a3eca2161b40bf2bf0c55c2b04c8b5962cada1
Jerwin Peria <jerwinperiait@gmail.com>
2026-02-12 22:19:04 +0800

    allowed unsplash image

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   images: {
-    domains: ["lh3.googleusercontent.com", "cdn.example.com", "unsplash.com"],
+    domains: [
+      "lh3.googleusercontent.com",
+      "cdn.example.com",
+      "images.unsplash.com",
+    ],
   },
 };
 
